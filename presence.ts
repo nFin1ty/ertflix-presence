0ebd22d5e07127d9e30c14c505c2ca77e1881a99
Stathis <16375254+nFin1ty@users.noreply.github.com>
2022-10-09 16:12:56 +0300

    Add player state info

--- presence.ts
+++ presence.ts
@@ -2,6 +2,8 @@ const presence = new Presence({
 	clientId: "1003670800089497680",
 });
 
+let poster = "";
+
 presence.on("UpdateData", async () => {
 
 	const presenceData: PresenceData = {
@@ -9,11 +11,29 @@ presence.on("UpdateData", async () => {
 	};
 
 	if (document.location.href.includes("/vod/")) {
-		let poster = document.querySelector<HTMLImageElement>("img[data-role='poster']").src;
 		let title = document.querySelector<HTMLSpanElement>("span[class^='AssetTitles__title']").textContent;
+		let year = document.querySelector<HTMLUListElement>("ul[class^='VodDetails'] li").textContent;
 		presenceData.largeImageKey = poster;
-		presenceData.details = "Περιήγηση...";
-		presenceData.state = title;
+		presenceData.state = `${title} (${year})`;
+		presenceData.buttons = [
+			{
+				label: "Παρακολούθηση",
+				url: document.location.href,
+			},
+		];
+
+		if (!document.querySelector("div[data-test='insys-player']")) {
+			poster = document.querySelector<HTMLImageElement>("img[data-role='poster']").src;
+			presenceData.details = "Περιήγηση...";
+		} else {
+			let video = document.querySelector<HTMLMediaElement>("div[data-test='insys-player'] video");
+			
+			[, presenceData.endTimestamp] = presence.getTimestampsfromMedia(video);
+			//if (video.paused) delete presenceData.endTimestamp;
+			presenceData.smallImageKey = video.paused ? "pause" : "play";
+			presenceData.smallImageText = video.paused ? "Σε παύση" : "Αναπαραγωγή";
+			presenceData.details = "Βλέπει";
+		}
 	}
 
 	if (presenceData.details) presence.setActivity(presenceData);
